f0a75e74af7abe59c99b2ec8cf10c2eb05427a3b
Matthew R Quinn <MatthewRileyQuinn@gmail.com>
2021-01-20 13:27:06 -0800

    fixed string to bigInt conversion

--- src/EuclideanAdjustment.java
+++ src/EuclideanAdjustment.java
@@ -3,16 +3,19 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.math.BigInteger;
+
 public class EuclideanAdjustment {
-	
 	public static void main(String[] args) throws FileNotFoundException {
 		ArrayList<BigInteger> movementValues = readCharacters();
-		BigInteger target = movementValues.get(0);
-		movementValues.remove(0);
+		BigInteger target = getTarget(movementValues);
 		movementValues.sort(null);
+		// Size of the list(M values)
 		int size = movementValues.size();
+		
 		System.out.println("Target: " + target);
 		System.out.println(movementValues);
+		
+		//loops through and subtracts the values from the target until the m values are smaller than the target or until we run out of M values
 		for(int i = 0; i < size; i++) {
 			while(movementValues.get(size - i - 1).compareTo(target) == -1) {
 				target = target.subtract(movementValues.get(size-i - 1));
@@ -23,15 +26,32 @@ public class EuclideanAdjustment {
 		
 	}
 	
+	/**
+	 * gets the Bigints from the file
+	 * @return an arrayList containing the bigIntegers
+	 * @throws FileNotFoundException
+	 */
 	public static ArrayList<BigInteger> readCharacters() throws FileNotFoundException {
-		File fileName = new File("input.txt");
+		File fileName = new File("inputBig.txt");
 		Scanner in = new Scanner(fileName);
 		ArrayList<BigInteger> fileContent = new ArrayList<BigInteger>();
 		while(in.hasNextLine()) {
-			int temp = Integer.parseInt(in.nextLine());
-			fileContent.add(BigInteger.valueOf(temp));
+			BigInteger val = new BigInteger(in.nextLine());
+			fileContent.add(val);
 		}
 		return fileContent;
 	}
+	/**
+	 * Extracts the target value from the list and removes it from the list
+	 * @param list to extract the target from
+	 * @return the target value
+	 */
+	public static BigInteger getTarget(ArrayList<BigInteger> list) {
+		BigInteger target = list.get(0);
+		list.remove(0);
+		return target;
+	}
 	
 }
+
+//try to find a combination of M values that produce as small values as possible
