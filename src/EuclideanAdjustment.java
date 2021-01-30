ab22683a887b315080d8f276ec92aed0e096b7b6
Matthew R Quinn <MatthewRileyQuinn@gmail.com>
2021-01-30 14:11:30 -0800

    added comments and cleaned up some ugly code

--- src/EuclideanAdjustment.java
+++ src/EuclideanAdjustment.java
@@ -3,125 +3,156 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 import java.math.BigInteger;
-//-3927, is divisible by 550? no. -1190, divisible by 550? no. -1190, divisible by 550? no. -1190, divisible by 550? no.
-//revert back to -3927, subtract -3927 again. now subtract -1190, check, -1190, check, -1190 check. Repeat until divisible by 550
-// this only justifies at most 3 * -1190 and not the 5 we see in the solution
+
+/**
+ * Matthew Quinn
+ * ID: 026127164
+ * CECS 328 T/TH 11am
+ * This program will calculate the adjustments left or right to achieve some number "goal"
+ * The adjustments are fixed values provided in the input.txt file
+ * This version of the program is extremely limited and is only capable of solving variations of the problem where
+ * the solution uses exactly 3 of the provided adjustment values
+ *
+ * The program simplifies the initial problem and attempts to find a case where m1*val1 + m2*val2 + m3*val3 = 1
+ * then multiplies both sides by goal, giving the solution for reaching goal.
+ */
 public class EuclideanAdjustment {
     public static void main(String[] args) throws IOException {
+        // gets the values from input.txt
         ArrayList<BigInteger> vals = readCharacters();
+        // saves a copy of the unsorted list
+        ArrayList<BigInteger> originalValues = (ArrayList<BigInteger>) vals.clone();
+        // the number we want to reach
         BigInteger goal = getGoal(vals);
+        // sets up our currentValue used in the solution attempt
         BigInteger currentVal = new BigInteger("0");
+        //sorts the array
         vals.sort(null);
-        // Size of the list(M values)
-        int size = vals.size();
-        // Counter to keep track of how many times each operation was done
-        BigInteger[] counter = new BigInteger[size];
-        for (int i = 0; i < size; i++) {
-            counter[i] = new BigInteger("0");
-        }
-        System.out.println("Target: " + goal);
-        System.out.println(vals);
 
+        // Counter to keep track of how many times each operation was done
+        BigInteger[] counter = new BigInteger[3];
+
+        //main loop to try different combinations of numbers
         ArrayList<BigInteger> shortVals = new ArrayList<>();
         boolean flag = false;
         for(int i = 0; i < vals.size()-2; i++){
+            if(flag) break;
             for(int j = 1; j < vals.size()-1; j++){
+                if(flag) break;
                 for(int k = 2; k < vals.size(); k++){
-                    for (int n = 0; n < size; n++) {
+                    if(flag) break;
+                    for (int n = 0; n < 3; n++) {
                         counter[n] = new BigInteger("0");
                     }
-                    if(i != j && j != k){
-                        shortVals.clear();
-                        shortVals.add(vals.get(i));
-                        shortVals.add(vals.get(j));
-                        shortVals.add(vals.get(k));
-                        if(staticAttemp(shortVals, counter, goal)){
-                            flag = true;
-                            break;
-                        }
+                    shortVals.clear();
+                    shortVals.add(vals.get(i));
+                    shortVals.add(vals.get(j));
+                    shortVals.add(vals.get(k));
+                    if(limitedAttempt(shortVals, counter, goal, originalValues)){
+                        flag = true;
                     }
-
                 }
-                if(flag) break;
             }
-            if(flag) break;
         }
-       /* shortVals.add(vals.get(5));//smallest
-        shortVals.add(vals.get(6));//medium
-        shortVals.add(vals.get(12));//largest
-        System.out.println(staticAttemp(shortVals, counter, goal));*/
-
-        //inputTest();
+        if(!flag){
+            System.out.println("program failed to find a solution :(");
+        }else{
+            System.out.println("program found a solution :D");
+            
+        }
 
     }
 
-    public static boolean staticAttemp(ArrayList<BigInteger> vals, BigInteger counter[], BigInteger goal) throws IOException {
+    /**
+     *
+     * @param vals the numbers we are attempting to use to find a solution
+     * @param counter the number of times each vale was used
+     * @param goal the number we want to reach in the end
+     * @param originalValues the original set of values before sorting
+     * @return true if it finds a solution, false if it doesnt
+     * @throws IOException
+     */
+    public static boolean limitedAttempt(ArrayList<BigInteger> vals, BigInteger counter[], BigInteger goal, ArrayList<BigInteger> originalValues) throws IOException {
+        // loops keeps track of the number of loops the program has gone though, used to break after 5,000 loops
         BigInteger loops = new BigInteger("0");
+        // the current value we are working with, starts at 0
         BigInteger current = new BigInteger("0");
+        // are target is One as once we solve for one we can multiply all counts by goal to get the actual number of
+        //movements. This reduces the total amount of operations we need to do
         BigInteger one = new BigInteger("1");
         while(current.mod(vals.get(0)).compareTo(one)!=0){
-            if(loops.compareTo(new BigInteger("10000")) == 1){
-
-                break;
+            //if the number of loops is larger than 5000 return false. Meaning the program failed.
+            if(loops.compareTo(new BigInteger("5000")) == 1){
+                return false;
             }
+            // subtracts the largest of the 3 values we passed in
             current = current.subtract(vals.get(2));
+            //updates the counter
             counter[2] = counter[2].subtract(one);
             BigInteger temp = current;
+            //checks if this is a valid solution set up
             if(current.mod(vals.get(0)).compareTo(one)==0) {
-                System.out.println("Solved");
                 break;
             }
-
-            for(int i = 0; i < 7; i++){
+            //adds the medium number we passed in 10 times
+            for(int i = 0; i < 10; i++){
                 current = current.subtract(vals.get(1));
+                //updates the counter
                 counter[1] = counter[1].subtract(one);
+                //checks if this is a valid solution set up
                 if(current.mod(vals.get(0)).compareTo(one)==0) {
                     break;
                 }
             }
             if(current.mod(vals.get(0)).compareTo(one)==0) {
-                System.out.println("Solved");
                 break;
             }
-
+            //updates the counter
             counter[1] = new BigInteger("0");
+            //if the middle number didnt reach a solution we undo the changes it made.
             current = temp;
 
             loops = loops.add(one);
 
         }
-        if(loops.compareTo(new BigInteger("10000")) == 1) {
+        //returns a failure to find a solution if hte loops go over 5000
+        if(loops.compareTo(new BigInteger("5000")) == 1) {
             return false;
         }
-        System.out.println("current " + current);
-        System.out.println("loops: " + loops);
+        //calculates the shifts for our smallest value
         counter[0] = ((current.subtract(one)).divide(vals.get(0))).abs();
-
-        writeAnswerToFile(vals, counter, goal);
-        printArr(counter,goal);
-        System.out.println("Added together " + counter[0].multiply(vals.get(0)).add(counter[1].multiply(vals.get(1))).add(counter[2].multiply(vals.get(2))));
+        for(int i = 0; i < counter.length; i++){
+            counter[i] = counter[i].multiply(goal);
+        }
+        //writes the answer to output.txt
+        writeAnswerToFile(vals, counter, originalValues);
+        //returns true meaning the program successfully found a solution
         return true;
     }
-    public static void writeAnswerToFile(ArrayList<BigInteger> vals, BigInteger counter[], BigInteger goal) throws IOException {
-        FileWriter outPut = new FileWriter("outputSelfTest.txt");
-        for(int i = 0; i < vals.size(); i++){
-            outPut.write(vals.get(i).toString() + " " + counter[i].multiply(goal).toString() + "\n");
-        }
-        outPut.close();
-    }
-    public static int[] attempt(ArrayList<BigInteger> vals, BigInteger currentVal, int counter[]) {
-    	BigInteger one = new BigInteger("1");
-    	for(int i = 0; i < vals.size();i++) {
-    		if(vals.get(i).mod(currentVal) == one) {
-    			return counter;
-    		}
 
-    	}
-    	return null;
+    /**
+     * Writes the solution to output.txt
+     * @param vals the values used in the solution
+     * @param counter the number of times each value was used
+     * @param originalValues the unsorted original list, used to set the list back into its original order
+     * @throws IOException throws IOExecption if an error occurs on file write
+     */
+    public static void writeAnswerToFile(ArrayList<BigInteger> vals, BigInteger counter[], ArrayList<BigInteger> originalValues) throws IOException {
+        //converts the order of the list back to the original input(before sorting)
+        FileWriter outPut = new FileWriter("output.txt");
+        for(int i = 0; i < originalValues.size(); i++){
+            for(int j = 0; j < vals.size(); j++){
+                if(vals.get(j).compareTo(originalValues.get(i)) == 0){
+                    outPut.write(vals.get(j).toString() + " " + counter[j].toString() + "\n");
+                }
+            }
+        }
+
+
+
+        outPut.close();
     }
 
     /**
@@ -153,82 +184,5 @@ public class EuclideanAdjustment {
         list.remove(0);
         return target;
     }
-
-    public static void printArr(BigInteger[] arr, BigInteger goal) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i].multiply(goal));
-        }
-    }
-
-    public static void inputTest() throws IOException {
-        FileWriter outPut = new FileWriter("OutputTest.txt");
-        int smallest = Integer.MAX_VALUE;
-        int x = 0;
-        int a = 5;//-1190
-        int b = 27050;//550
-        int c = 3787;//-3927
-        while (a > 0 || b > 0 || c > 0) {
-            if (a > 0) {
-                x -= 1190;
-                a--;
-            }
-            if (b > 0) {
-                x += 550;
-                b--;
-            }
-            if (c > 0) {
-                x -= 3927;
-                c--;
-            }
-            if(x < smallest){
-                smallest = x;
-            }
-            String str = "";
-            if(c == 0){
-                String temp = "";
-                temp += "value when c ends is = " + x + "\n";
-                outPut.write(temp);
-                c--;
-            }
-            if(a == 0){
-                String temp = "";
-                temp += "value when a ends is = " + x + "\n";
-                outPut.write(temp);
-                a--;
-            }
-            int testVal = 550;
-            if(Math.abs(gcd(testVal, x)) != 1){
-                String temp = "";
-                temp += testVal + " and X have a a gcd of " + gcd(testVal, x) + "\n";
-                //outPut.write(temp);
-            }
-            str += x;
-            outPut.write("x = " + x + "\n");
-
-        }
-        System.out.println("X = " + x);
-        System.out.println("smallest = " + smallest);
-        outPut.close();
-    }
-
-    public static int gcd(int a, int b) {
-        if (b==0) return a;
-        return gcd(b,a%b);
-    }
 }
 
-//try to find a combination of M values that produce as small values as possible
-// Find sets of numbers that have a GCD with one another. A and B have a non 1 GCD, A and C have a non 1 GCD, C and B have a non 1 GCD
-// Transitivity does not apply to GCD. Must manually compare to find the desired set
-// Find set {a, b, c , n} then do computation on those until we get a value T that had a non 1 GCD with values a-n
-//(1190 * 92195) + 18439 has a non 1 GCD for both 550 and 3927. Both of these have a GCD of 11
-
-
-//All x values are a multiple of T
-// new formula 1 = [m1 * x1] + [m2 * x2] + [mn * xn]
-// once 1 is found multiply the x movements by goal to get the required movements to find goal
-// once we find a way to select the useful numbers from the list of M we use the smallest one for adding and the rest for subtracting
-
-//Current tasks
-//how to select which numbers to use from list
-//how to know when to stop subtracting. Why is 1190 subtracted EXACTLY 5 times
