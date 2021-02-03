3f8ff3caa89d12f21a739ba340f6d9b5412ea4e1
Matthew R Quinn <MatthewRileyQuinn@gmail.com>
2021-02-03 13:51:13 -0800

    uses 4 numbers now,
    input  works, however input1 ends up with a very large multiple of the goal

--- src/Main.java
+++ src/Main.java
@@ -34,35 +34,39 @@ public class Main {
         vals.sort(null);
         System.out.println(vals);
         // Counter to keep track of how many times each operation was done
-        BigInteger[] counter = new BigInteger[3];
+        BigInteger[] counter = new BigInteger[4];
 
         //main loop to try different combinations of numbers
         ArrayList<BigInteger> shortVals = new ArrayList<>();
         //breaks if a solution has already been found
         boolean flag = false;
-        for (int i = 0; i < vals.size() - 2; i++) {
+        for (int i = 0; i < vals.size() - 3; i++) {
             //breaks if a solution has already been found
             if (flag) break;
-            for (int j = i + 1; j < vals.size() - 1; j++) {
+            for (int j = i + 1; j < vals.size() - 2; j++) {
                 //breaks if a solution has already been found
                 if (flag) break;
-                for (int k = j + 1; k < vals.size(); k++) {
-                    //breaks if a solution has already been found
+                for (int k = j + 1; k < vals.size()-1; k++) {
                     if (flag) break;
-                    for (int n = 0; n < 3; n++) {
-                        counter[n] = new BigInteger("0");
-                    }
-                    //clears the list
-                    shortVals.clear();
-                    //smallest of the 3 values
-                    shortVals.add(vals.get(i));
-                    //middle of the 3 values
-                    shortVals.add(vals.get(j));
-                    //largest of the 3 values
-                    shortVals.add(vals.get(k));
-                    if (limitedAttempt(shortVals, counter)) {
-                        //sets flag to true to allow for break out of loop
-                        flag = true;
+                    for (int l = k + 1; l < vals.size(); l++) {
+                        //breaks if a solution has already been found
+                        if (flag) break;
+                        for (int n = 0; n < counter.length; n++) {
+                            counter[n] = new BigInteger("0");
+                        }
+                        //clears the list
+                        shortVals.clear();
+                        //smallest of the 3 values
+                        shortVals.add(vals.get(i));
+                        //middle of the 3 values
+                        shortVals.add(vals.get(j));
+                        //largest of the 3 values
+                        shortVals.add(vals.get(k));
+                        shortVals.add(vals.get(l));
+                        if (limitedAttempt(shortVals, counter)) {
+                            //sets flag to true to allow for break out of loop
+                            flag = true;
+                        }
                     }
                 }
 
@@ -71,12 +75,18 @@ public class Main {
         if (!flag) {
             System.out.println("program failed to find a solution :(");
         } else {
+
             System.out.println("program found a solution :D");
             //multiplies counters by goal, before this multiplication counters solve for 1. Multiplying by goal
             //gives us the expected values we needed.
             for (int i = 0; i < counter.length; i++) {
                 counter[i] = counter[i].multiply(goal);
             }
+            BigInteger sum = new BigInteger("0");
+            for(int i = 0; i < shortVals.size(); i++){
+                sum = sum.add(counter[i].multiply(vals.get(i)));
+            }
+            System.out.println(sum);
             //writes the answer to output.txt
             writeAnswerToFile(shortVals, counter, originalValues);
         }
@@ -90,6 +100,11 @@ public class Main {
      * @throws IOException
      */
     public static boolean limitedAttempt(ArrayList<BigInteger> vals, BigInteger counter[]) {
+        int size = vals.size();
+        int largest = size-1;
+        int upperMid = size-2;
+        int lowerMid = size-3;
+        int smallest = size-4;
         // loops keeps track of the number of loops the program has gone though, used to break after 5,000 loops
         BigInteger loops = new BigInteger("0");
         // the current value we are working with, starts at 0
@@ -97,40 +112,53 @@ public class Main {
         // are target is One as once we solve for one we can multiply all counts by goal to get the actual number of
         //movements. This reduces the total amount of operations we need to do
         BigInteger one = new BigInteger("1");
-        System.out.println(vals.get(2) + " / " + vals.get(1));
-        BigInteger medNumLoopTimes = vals.get(2).divide(vals.get(1));
-        medNumLoopTimes = medNumLoopTimes.add(new BigInteger("15"));
-        int medLoopTermination = medNumLoopTimes.intValue();
-        System.out.println(medLoopTermination);
+        BigInteger upperMidLoops = vals.get(largest).divide(vals.get(upperMid));
+        BigInteger lowMidLoops = vals.get(upperMid).divide(vals.get(lowerMid));
+        int lowMidTermination = lowMidLoops.intValue();
+        lowMidTermination += 15;
+        int upperMidTermination = upperMidLoops.intValue();
+        upperMidTermination += 15;
         while (current.mod(vals.get(0)).compareTo(one) != 0) {
             //if the number of loops is larger than 5000 return false. Meaning the program failed.
             if (loops.compareTo(new BigInteger("5000")) == 1) {
                 return false;
             }
             // subtracts the largest of the 3 values we passed in
-            current = current.subtract(vals.get(2));
+            current = current.subtract(vals.get(largest));
             //updates the counter
-            counter[2] = counter[2].subtract(one);
+            counter[largest] = counter[largest].subtract(one);
             BigInteger temp = current;
             //checks if this is a valid solution set up
             if (current.mod(vals.get(0)).compareTo(one) == 0) {
                 break;
             }
             //adds the medium number we passed in 10 times
-            for (int i = 0; i < medLoopTermination; i++) {
-                current = current.subtract(vals.get(1));
+            for (int i = 0; i < upperMidTermination; i++) {
+                current = current.subtract(vals.get(upperMid));
                 //updates the counter
-                counter[1] = counter[1].subtract(one);
+                counter[upperMid] = counter[upperMid].subtract(one);
+
+                BigInteger localTemp = current;
+                for(int j = 0; j < lowMidTermination; j++){
+                    current = current.subtract(vals.get(lowerMid));
+                    counter[lowerMid] = counter[lowerMid].subtract(one);
+                    if (current.mod(vals.get(0)).compareTo(one) == 0) {
+                        break;
+                    }
+                }
+
                 //checks if this is a valid solution set up
                 if (current.mod(vals.get(0)).compareTo(one) == 0) {
                     break;
                 }
+                current = localTemp;
+                counter[lowerMid] = new BigInteger("0");
             }
             if (current.mod(vals.get(0)).compareTo(one) == 0) {
                 break;
             }
             //updates the counter
-            counter[1] = new BigInteger("0");
+            counter[upperMid] = new BigInteger("0");
             //if the middle number didnt reach a solution we undo the changes it made.
             current = temp;
 
@@ -144,6 +172,7 @@ public class Main {
         }
         //calculates the shifts for our smallest value
         counter[0] = ((current.subtract(one)).divide(vals.get(0))).abs();
+
         //returns true meaning the program successfully found a solution
         return true;
     }
@@ -157,6 +186,7 @@ public class Main {
      * @throws IOException throws IOExecption if an error occurs on file write
      */
     public static void writeAnswerToFile(ArrayList<BigInteger> vals, BigInteger counter[], ArrayList<BigInteger> originalValues) throws IOException {
+
         //converts the order of the list back to the original input(before sorting)
         FileWriter outPut = new FileWriter("output.txt");
         for (int i = 0; i < originalValues.size(); i++) {
@@ -169,6 +199,7 @@ public class Main {
 
 
         outPut.close();
+
     }
 
     /**
@@ -178,7 +209,7 @@ public class Main {
      * @throws FileNotFoundException
      */
     public static ArrayList<BigInteger> readCharacters() throws FileNotFoundException {
-        File fileName = new File("input.txt");
+        File fileName = new File("input1.txt");
         Scanner in = new Scanner(fileName);
         ArrayList<BigInteger> fileContent = new ArrayList<BigInteger>();
         while (in.hasNextLine()) {
